6b658f713d90dd9f1c65443c05ef6cbaf78cd5d0
Moses-1844 <grahamjuma2001@gmail.com>
2026-07-28 16:18:21 +0300

    adminhh

--- fundi_backend/wsgi.py
+++ fundi_backend/wsgi.py
@@ -8,15 +8,6 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-# existing imports...
-
-if os.environ.get("RUN_CREATE_USERS") == "1":
-    try:
-        from create_railway_users import main as create_users_main
-        create_users_main()
-    except Exception:
-        pass
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fundi_backend.settings')
